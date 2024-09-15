f4c8eb6f059d39ce952fdb303ac5de3b69866d8b
IlliaSRadency <illia.s@radency.com>
2024-09-16 02:59:44 +0300

    Feat/Implemented started drag n drop

--- src/components/TabContainer.tsx
+++ src/components/TabContainer.tsx
@@ -1,4 +1,24 @@
-import { useDroppable } from "@dnd-kit/core"
+import {
+   closestCenter,
+   DndContext,
+   DragEndEvent,
+   KeyboardSensor,
+   PointerSensor,
+   TouchSensor,
+   useDroppable,
+   useSensor,
+   useSensors,
+} from "@dnd-kit/core"
+import {
+   restrictToHorizontalAxis,
+   restrictToWindowEdges,
+} from "@dnd-kit/modifiers"
+import {
+   arrayMove,
+   horizontalListSortingStrategy,
+   SortableContext,
+   sortableKeyboardCoordinates,
+} from "@dnd-kit/sortable"
 import { Divider } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
@@ -27,13 +47,33 @@ export default function TabContainer() {
       setVisibleTabs,
       handleRemoveTab,
       setPinnedTabs,
+      setPersistentData,
    } = useTabContext()
 
-   const { isOver, setNodeRef } = useDroppable({
+   const { setNodeRef } = useDroppable({
       id: "droppable",
    })
 
+   const sensors = useSensors(
+      useSensor(PointerSensor, {
+         activationConstraint: {
+            delay: 150,
+            tolerance: 5,
+         },
+      }),
+      useSensor(TouchSensor, {
+         activationConstraint: {
+            delay: 2000,
+            tolerance: 5,
+         },
+      }),
+      useSensor(KeyboardSensor, {
+         coordinateGetter: sortableKeyboardCoordinates,
+      })
+   )
+
    const [hoveredTab, setHoveredTab] = useState<number | null>(null)
+   const [dragStarted, setDragStarted] = useState<boolean>(false)
 
    useEffect(() => {
       const updateTabs = () => {
@@ -60,6 +100,50 @@ export default function TabContainer() {
 
    const handleMouseEnter = (id: number) => setHoveredTab(id)
    const handleMouseLeave = () => setHoveredTab(null)
+
+   function handleDragStart() {
+      setDragStarted(true)
+   }
+
+   function handleDragEnd(event: DragEndEvent) {
+      setDragStarted(false)
+      const { active, over } = event
+
+      if (over && active.id !== over.id) {
+         setVisibleTabs((items) => {
+            const oldIndex = items.findIndex((item) => item.id === active.id)
+            const newIndex = items.findIndex((item) => item.id === over.id)
+
+            if (oldIndex !== -1 && newIndex !== -1) {
+               const newVisibleTabs = arrayMove(items, oldIndex, newIndex)
+
+               const sortedPersistentData = [
+                  ...pinnedTabs,
+                  ...newVisibleTabs,
+                  ...overflowTabs,
+               ].map((tab) => {
+                  return persistentData.find((p) => p.id === tab.id) || tab
+               })
+
+               setPersistentData(sortedPersistentData)
+
+               const maxVisibleTabs = 13
+               const newOverflowTabs =
+                  sortedPersistentData.slice(maxVisibleTabs)
+               const updatedVisibleTabs = sortedPersistentData
+                  .slice(0, maxVisibleTabs)
+                  .filter((tab) => !tab.isPinned)
+
+               setVisibleTabs(updatedVisibleTabs)
+               setOverflowTabs(newOverflowTabs)
+
+               return updatedVisibleTabs
+            }
+
+            return items
+         })
+      }
+   }
 
    const renderPinnedTabs = () => {
       return pinnedTabs.map(({ icon, name, id }, index, tabsData) => {
@@ -129,6 +213,7 @@ export default function TabContainer() {
                   id={`tab-${id}`}
                   onRemoveTab={() => handleRemoveTab(id)}
                   hoveredTab={hoveredTab}
+                  dragStarted={dragStarted}
                >
                   {name}
                </TabElement>
@@ -149,24 +234,34 @@ export default function TabContainer() {
    }
 
    return (
-      <>
+      <DndContext
+         sensors={sensors}
+         collisionDetection={closestCenter}
+         onDragEnd={handleDragEnd}
+         onDragStart={handleDragStart}
+         modifiers={[restrictToWindowEdges, restrictToHorizontalAxis]}
+      >
          <div className="flex w-full" ref={setNodeRef}>
             {pinnedTabs.length > 0 && (
                <TabPinnedContainer>{renderPinnedTabs()}</TabPinnedContainer>
             )}
-
-            {loading ? null : (
-               <div id="tab-container" className="flex w-full">
-                  {visibleTabs.length > 0
-                     ? renderTabs(visibleTabs)
-                     : renderTabs(persistentData)}
-               </div>
-            )}
+            <SortableContext
+               items={visibleTabs.map((tab) => tab.id)}
+               strategy={horizontalListSortingStrategy}
+            >
+               {loading ? null : (
+                  <div id="tab-container" className="flex w-full">
+                     {visibleTabs.length > 0
+                        ? renderTabs(visibleTabs)
+                        : renderTabs(persistentData)}
+                  </div>
+               )}
+            </SortableContext>
 
             <TabDropdown overflowTabs={overflowTabs} />
          </div>
 
          <TabOptionsMenu />
-      </>
+      </DndContext>
    )
 }
